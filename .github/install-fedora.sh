30ed7ca0031ac1dfb5a98176912021ad321702eb
Steven Noonan <steven@uplinklabs.net>
2021-01-10 08:55:43 -0800

    install-fedora: use fastestmirror, and pipefail
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- .github/install-fedora.sh
+++ .github/install-fedora.sh
@@ -3,6 +3,7 @@
 # This is an install script for Fedora-specific packages.
 #
 set -ex
+set -o pipefail
 
 # Base build packages
 PACKAGES=(
@@ -19,5 +20,12 @@ PACKAGES=(
 	cmake
 )
 
+if ! grep '^fastestmirror' /etc/dnf/dnf.conf; then
+	echo 'fastestmirror=1' >> /etc/dnf/dnf.conf
+fi
+
+dnf clean all | cat
 dnf update -y | cat
 dnf install -y "${PACKAGES[@]}" | cat
+
+exit 0
